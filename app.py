9908ec62f71f19c618411c9ad270043908e54d53
mateusDbarros <mb685212@gmail.com>
2025-03-14 16:53:24 -0300

    Updating the code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from sqlalchemy import SQLAlchemy
 from urllib.parse import quote_plus
 
@@ -14,3 +14,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{USERNAME}:{PASSWORD}@{HO
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    field = db.Column(db.String(100), nullable=False)
+    invest = db.Column(db.Float, nullable=False)
+    
+    def to_json(self):
+        return {'id': self.id, 'name':self.name, 'field':self.field, 'invest':self.invest}
+
+with app.app_context:
+    db.create_all()
+    
